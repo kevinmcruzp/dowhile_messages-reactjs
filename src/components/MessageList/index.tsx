1c05169484ce0c78008ae4c0d4fb1b45d1f61316
Kevin Cruz <kevin_mcp@outlook.com>
2021-11-15 15:29:24 -0300

    MessageList component finished

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,5 +1,43 @@
+import styles from './styles.module.scss'
+
+import logoImg from '../../assets/logo.svg'
+
 export function MessageList() {
   return (
-    <h1>MessageList</h1>
+    <div className={styles.messageListWrapper}>
+      <img src={logoImg} alt="DoWhile2021" />
+
+      <ul className={styles.messageList}>
+        <li className={styles.message}>
+          <p className={styles.messageContent}>Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamoo pra cima! 🔥🔥.</p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img src="https://github.com/KevinMCruzP.png" alt="Kevin Cruz" />
+            </div>
+            <span>Kevin Cruz</span>
+          </div>
+        </li>
+
+        <li className={styles.message}>
+          <p className={styles.messageContent}>Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamoo pra cima! 🔥🔥.</p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img src="https://github.com/KevinMCruzP.png" alt="Kevin Cruz" />
+            </div>
+            <span>Kevin Cruz</span>
+          </div>
+        </li>
+
+        <li className={styles.message}>
+          <p className={styles.messageContent}>Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamoo pra cima! 🔥🔥.</p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img src="https://github.com/KevinMCruzP.png" alt="Kevin Cruz" />
+            </div>
+            <span>Kevin Cruz</span>
+          </div>
+        </li>
+      </ul>
+    </div>
   )
 }
